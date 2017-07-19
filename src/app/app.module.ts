f7e90af136415c83e6678a317a7ec4eec307624c
kny <nymsc@mscbrain.com>
2017-07-19 19:49:59 +0900

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import "froala-editor/js/froala_editor.pkgd.min.js";
+import { FroalaEditorModule, FroalaViewModule } from 'angular-froala-wysiwyg';
+
+import { CKEditorModule } from "../../node_modules/ng2-ckeditor";
 
 import { AppComponent } from './app.component';
+import { FroalaComponent } from './froala/froala.component';
+import { CkComponent } from './ck/ck.component';
+
+import { AppRouterModule } from  './app-router/app-router.module';
+import { DownloadComponent } from './download/download.component';
+
+import { MockService } from './mock.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FroalaComponent,
+    CkComponent,
+    DownloadComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRouterModule,
+
+    FroalaEditorModule.forRoot(),
+    FroalaViewModule.forRoot(),
+    CKEditorModule
   ],
-  providers: [],
+  providers: [MockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
